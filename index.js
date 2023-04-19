deb389fa28c8e371262f4b59dc9a4d0614f703b2
Parth <parthbhuva97@gmail.com>
2023-04-19 22:05:58 +0530

    CORS

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 // app.use(cors({
 //   origin: 'http://127.0.0.1:5173',
 //   credentials: true,
